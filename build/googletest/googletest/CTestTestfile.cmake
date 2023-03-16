d26d8bbee022d718bba71f8ec06e91e109ef97a0
Eksal <eksal@xububtu.myguest.virtualbox.org>
2023-03-16 12:38:14 +0100

    changes

---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eksal/Desktop/cpp/googletest/googletest
+# Build directory: /home/eksal/Desktop/cpp/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
